1ceb0c80280d6255199695c429013d6682d02d81
Javed Hussain <jh880232@gmail.com>
2025-10-26 15:56:11 +0530

    Update route handling for serving frontend

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(authRoute);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get(/^\/.*$/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
