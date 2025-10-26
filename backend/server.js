756337d46a17457f5b001361a6b88f582eb2dc77
Javed Hussain <jh880232@gmail.com>
2025-10-26 16:03:09 +0530

    Change static file serving from build to dist

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,10 @@ app.use(express.json());
 app.use(cors(option));
 app.use(authRoute);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get(/^\/.*$/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 app.get("/",(req,res)=>{
