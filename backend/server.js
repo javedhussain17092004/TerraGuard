83a5a2f32d43e1d7c4719e1b67ef83dbf8bdc4bb
Javed Hussain <jh880232@gmail.com>
2025-10-26 15:42:32 +0530

    updating static
    Serve static files from the frontend build directory and handle all other routes by sending the index.html file.

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,17 @@ app.use(express.json());
 app.use(cors(option));
 app.use(authRoute);
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
 app.get("/",(req,res)=>{
     console.log("helo world");
 })
 
-app.listen(port,()=>{
+app.listen(port,"0.0.0.0",()=>{
     console.log("app is listening at port 8080");
     const connect=async()=>{
         await mongoose.connect(process.env.MONGODB_URL).then((res)=>{
